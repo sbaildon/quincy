3973f8a1a2f442cba0bcf41cfe2d1c02c9366bc1
Sean Baildon <sean@baildon.co>
2016-06-19 13:34:47 +0100

    Check if websocket connection is actually established

--- bot.go
+++ bot.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"os"
 	"os/signal"
-	"flag"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 
@@ -29,12 +30,27 @@ func main() {
 
 	handlers.SetupHandlers(dg)
 
+	fmt.Println("Opening websocket connection")
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("Unable to open discord connection")
+		return
 	}
 
-	fmt.Println("Connected. Maybe?")
+	time.Sleep(1000 * time.Millisecond)
+
+	attempts := 0
+	for !dg.DataReady {
+		attempts++
+		if attempts == 10 {
+			fmt.Println("Timeout exceeded")
+			return
+		}
+		time.Sleep(1000 * time.Millisecond)
+		fmt.Println("Waiting for connection to be established")
+	}
+
+	fmt.Println("Connected")
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
